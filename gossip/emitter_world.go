a33c927158aae6145dd227cdf99ead54796cd6dd
MikeO <10thmountain@zoho.com>
2024-11-28 01:44:30 +0800

    Update emitter_world.go

--- gossip/emitter_world.go
+++ gossip/emitter_world.go
@@ -7,12 +7,12 @@ import (
 	"github.com/Fantom-foundation/lachesis-base/inter/idx"
 	"github.com/ethereum/go-ethereum/core/types"
 
-	"github.com/Fantom-foundation/go-opera/gossip/emitter"
-	"github.com/Fantom-foundation/go-opera/inter"
-	"github.com/Fantom-foundation/go-opera/inter/state"
-	"github.com/Fantom-foundation/go-opera/utils/wgmutex"
-	"github.com/Fantom-foundation/go-opera/valkeystore"
-	"github.com/Fantom-foundation/go-opera/vecmt"
+	"github.com/mrmikeo/Xpense/gossip/emitter"
+	"github.com/mrmikeo/Xpense/inter"
+	"github.com/mrmikeo/Xpense/inter/state"
+	"github.com/mrmikeo/Xpense/utils/wgmutex"
+	"github.com/mrmikeo/Xpense/valkeystore"
+	"github.com/mrmikeo/Xpense/vecmt"
 )
 
 type emitterWorldProc struct {
