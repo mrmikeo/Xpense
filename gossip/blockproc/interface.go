818b78f2ace5f0ef60b923da767ae9d2567750d5
MikeO <10thmountain@zoho.com>
2024-11-28 02:25:05 +0800

    Update interface.go

--- gossip/blockproc/interface.go
+++ gossip/blockproc/interface.go
@@ -5,11 +5,11 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/params"
 
-	"github.com/Fantom-foundation/go-opera/evmcore"
-	"github.com/Fantom-foundation/go-opera/inter"
-	"github.com/Fantom-foundation/go-opera/inter/iblockproc"
-	"github.com/Fantom-foundation/go-opera/inter/state"
-	"github.com/Fantom-foundation/go-opera/opera"
+	"github.com/mrmikeo/Xpense/evmcore"
+	"github.com/mrmikeo/Xpense/inter"
+	"github.com/mrmikeo/Xpense/inter/iblockproc"
+	"github.com/mrmikeo/Xpense/inter/state"
+	"github.com/mrmikeo/Xpense/opera"
 )
 
 type TxListener interface {
